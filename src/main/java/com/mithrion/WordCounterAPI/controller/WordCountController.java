6bb86140100c67751b12557f8e95e49c86533998
Mithrion <alexmuthenya100@gmail.com>
2026-03-06 11:12:30 +0300

    feat: implement WordCountController with REST endpoint

---
+++ src/main/java/com/mithrion/WordCounterAPI/controller/WordCountController.java
@@ -0,0 +1,30 @@
+package com.mithrion.WordCounterAPI.controller;
+
+
+import com.mithrion.WordCounterAPI.model.WordCountResponse;
+import com.mithrion.WordCounterAPI.service.WordCountService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/wordcount")
+public class WordCountController {
+
+    private final WordCountService wordCountService;
+
+    public WordCountController(WordCountService wordCountService) {
+        this.wordCountService = wordCountService;
+    }
+
+    @PostMapping
+    public WordCountResponse countWord(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("word") String word
+    ) throws Exception {
+
+        return wordCountService.countWord(file, word);
+    }
+}
