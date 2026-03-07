ab57525d295b1d9964e0899ea2de124114284d9f
Mithrion <alexmuthenya100@gmail.com>
2026-03-07 13:09:02 +0300

    feat(service): implement WordCountService with strategy selection and comparison

--- src/main/java/com/mithrion/WordCounterAPI/service/WordCountService.java
+++ src/main/java/com/mithrion/WordCounterAPI/service/WordCountService.java
@@ -2,34 +2,38 @@ package com.mithrion.WordCounterAPI.service;
 
 
 import com.mithrion.WordCounterAPI.model.WordCountResponse;
+import com.mithrion.WordCounterAPI.strategy.MultiThreadWordCounter;
 import com.mithrion.WordCounterAPI.strategy.SingleThreadWordCounter;
+import com.mithrion.WordCounterAPI.strategy.WordCounterStrategy;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.io.FileOutputStream;
+
 
 @Service
 public class WordCountService {
 
-    private final SingleThreadWordCounter counter = new SingleThreadWordCounter();
+    private final SingleThreadWordCounter singleThreadWordCounter;
+    private final MultiThreadWordCounter multiThreadWordCounter;
 
-    public WordCountResponse countWord(MultipartFile multipartFile, String word) throws Exception {
+    public WordCountService(SingleThreadWordCounter single, MultiThreadWordCounter multi) {
+        this.singleThreadWordCounter = single;
+        this.multiThreadWordCounter = multi;
+    }
 
+    public WordCountResponse countWordOccurrences(MultipartFile multipartFile, String word, String mode) throws Exception {
         File file = convertToFile(multipartFile);
+        WordCounterStrategy strategy = mode.equalsIgnoreCase("multi")
+                ? multiThreadWordCounter
+                : singleThreadWordCounter;
 
-        long start = System.currentTimeMillis();
-
-        int count = counter.CountWord(file, word);
-
-        long end = System.currentTimeMillis();
-
-        return new WordCountResponse(
-                word,
-                count,
-                "single",
-                end - start
-        );
+        long startTime = System.currentTimeMillis();
+        int count = strategy.CountWord(file, word);
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        file.delete();
+        return new WordCountResponse(word, count, mode, duration);
     }
 
     private File convertToFile(MultipartFile multipartFile) throws Exception {
